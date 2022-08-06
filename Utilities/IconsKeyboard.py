5fab46493f5672a72b20c7ce311c80c4d590dea8
Freaku17 <92618708+Freaku17@users.noreply.github.com>
2022-08-06 12:25:18 +0530

    Update IconsKeyboard for 1.7

--- Utilities/IconsKeyboard.py
+++ Utilities/IconsKeyboard.py
@@ -1,5 +1,4 @@
-"""Defines simple icon keyboard."""
-#Made by: @[Just] Freak#4999 / Freaku
+#Made by: Freaku / @[Just] Freak#4999
 
 # • Icon Keyboard •
 # Make your chats look even more cooler!
@@ -9,7 +8,7 @@
 
 
 
-# ba_meta require api 6
+# ba_meta require api 7
 
 from __future__ import annotations
 
@@ -17,17 +16,9 @@ from typing import TYPE_CHECKING
 
 import ba
 from _ba import charstr as uwu
-from ba._enums import SpecialChar
 
 if TYPE_CHECKING:
     from typing import Any, Optional, Dict, List, Tuple,Type, Iterable
-
-__version__ ='1' #done
-
-# I spent quite some time, figuring out new icons and typing this ....
-# Change credits = Instant DEATH!
-
-
 
 
 
@@ -39,34 +30,34 @@ __version__ ='1' #done
 class IconKeyboard_byFreaku(ba.Keyboard):
     """Keyboard go brrrrrrr"""
     name = 'Icons by \ue048Freaku'
-    chars = [(uwu(SpecialChar.TICKET),
-            uwu(SpecialChar.CROWN),
-            uwu(SpecialChar.DRAGON),
-            uwu(SpecialChar.SKULL),
-            uwu(SpecialChar.HEART),
-            uwu(SpecialChar.FEDORA),
-            uwu(SpecialChar.HAL),
-            uwu(SpecialChar.YIN_YANG),
-            uwu(SpecialChar.EYE_BALL),
-            uwu(SpecialChar.HELMET),
-            uwu(SpecialChar.OUYA_BUTTON_U)),
-            (uwu(SpecialChar.MUSHROOM),
-            uwu(SpecialChar.NINJA_STAR),
-            uwu(SpecialChar.VIKING_HELMET),
-            uwu(SpecialChar.MOON),
-            uwu(SpecialChar.SPIDER),
-            uwu(SpecialChar.FIREBALL),
-            uwu(SpecialChar.MIKIROG),
-            uwu(SpecialChar.OUYA_BUTTON_O),
-            uwu(SpecialChar.LOCAL_ACCOUNT),
-            uwu(SpecialChar.LOGO)),
-            (uwu(SpecialChar.TICKET),
-            uwu(SpecialChar.FLAG_INDIA),
-            uwu(SpecialChar.OCULUS_LOGO),
-            uwu(SpecialChar.STEAM_LOGO),
-            uwu(SpecialChar.NVIDIA_LOGO),
-            uwu(SpecialChar.GAME_CENTER_LOGO),
-            uwu(SpecialChar.GOOGLE_PLAY_GAMES_LOGO),
-            uwu(SpecialChar.OUYA_BUTTON_A))]
+    chars = [(uwu(ba.SpecialChar.TICKET),
+            uwu(ba.SpecialChar.CROWN),
+            uwu(ba.SpecialChar.DRAGON),
+            uwu(ba.SpecialChar.SKULL),
+            uwu(ba.SpecialChar.HEART),
+            uwu(ba.SpecialChar.FEDORA),
+            uwu(ba.SpecialChar.HAL),
+            uwu(ba.SpecialChar.YIN_YANG),
+            uwu(ba.SpecialChar.EYE_BALL),
+            uwu(ba.SpecialChar.HELMET),
+            uwu(ba.SpecialChar.OUYA_BUTTON_U)),
+            (uwu(ba.SpecialChar.MUSHROOM),
+            uwu(ba.SpecialChar.NINJA_STAR),
+            uwu(ba.SpecialChar.VIKING_HELMET),
+            uwu(ba.SpecialChar.MOON),
+            uwu(ba.SpecialChar.SPIDER),
+            uwu(ba.SpecialChar.FIREBALL),
+            uwu(ba.SpecialChar.MIKIROG),
+            uwu(ba.SpecialChar.OUYA_BUTTON_O),
+            uwu(ba.SpecialChar.LOCAL_ACCOUNT),
+            uwu(ba.SpecialChar.LOGO)),
+            (uwu(ba.SpecialChar.TICKET),
+            uwu(ba.SpecialChar.FLAG_INDIA),
+            uwu(ba.SpecialChar.OCULUS_LOGO),
+            uwu(ba.SpecialChar.STEAM_LOGO),
+            uwu(ba.SpecialChar.NVIDIA_LOGO),
+            uwu(ba.SpecialChar.GAME_CENTER_LOGO),
+            uwu(ba.SpecialChar.GOOGLE_PLAY_GAMES_LOGO),
+            uwu(ba.SpecialChar.ALIBABA_LOGO))]
     nums = []
     pages: Dict[str, Tuple[str, ...]] = {}
