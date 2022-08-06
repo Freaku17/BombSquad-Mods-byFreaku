86682ff573f496000248b6084827e119fc1d7e6e
Freaku17 <92618708+Freaku17@users.noreply.github.com>
2022-08-06 11:36:32 +0530

    Update TowerD-Unlocked for 1.7

--- Utilities/TowerD-Unlocked.py
+++ Utilities/TowerD-Unlocked.py
@@ -1,24 +1,18 @@
-#Made by @[Just] Freak#4999 / Freaku
+#By Freaku / @[Just] Freak#4999
 
-# ba_meta require api 6
-
-from __future__ import annotations
-
-from typing import TYPE_CHECKING
 
 import ba
-import _ba
 from bastd.maps import TowerD
 
-if TYPE_CHECKING:
-    from typing import List, Tuple, Any, Optional
 
 @classmethod
-def new_play_types(cls) -> List[str]:
+def new_play_types(cls):
     """return valid play types for this map."""
     return ['melee', 'keep_away', 'team_flag', 'king_of_the_hill']
 
+
+# ba_meta require api 7
 # ba_meta export plugin
 class byFreaku(ba.Plugin):
-    def on_app_launch(self):
+    def on_app_running(self):
         TowerD.get_play_types = new_play_types
