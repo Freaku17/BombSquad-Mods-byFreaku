9dff2945be0055b3628ea75474be3be501668366
! Freaku <92618708+Freaku17@users.noreply.github.com>
2023-12-16 23:06:45 +0530

    Update icons_keyboard.py
    • Added ALL icons
    • Will automatically add new icons introduced in-game in future
    • Fixed errors; no characters present in `num` & `pages` list
    • Rename file to lowercase: To follow new schema according to using mods via Workspace

---
+++ Utilities/icons_keyboard.py
@@ -0,0 +1,34 @@
+# Made by your friend: Freaku
+
+# • Icon Keyboard •
+# Make your chats look even more cooler!
+# Make sure "Always Use Internal Keyboard" is ON
+# Double tap the space to change between keyboards...
+# Tap bottom-left bomb button to cycle through different icons
+
+
+# ba_meta require api 8
+
+import babase
+from babase import charstr
+
+list_of_icons = [i for i in babase.SpecialChar]
+list_of_icons = [charstr(i) for i in list_of_icons]
+list_of_icons.reverse()
+
+for i in range(26 - (len(list_of_icons) % 26)):
+    list_of_icons.append('‎')
+
+
+# ba_meta export keyboard
+class IconKeyboard(babase.Keyboard):
+    """Keyboard go brrrrrrr"""
+    name = 'Icons by \ue048Freaku'
+    chars = [(list_of_icons[0:10]),
+             (list_of_icons[10:19]),
+             (list_of_icons[19:26])]
+    nums = ['‎' for i in range(26)]
+    pages = {
+        f'icon{i//26+1}': tuple(list_of_icons[i:i+26])
+        for i in range(26, len(list_of_icons), 26)
+    }
